c01367460e449ce2ea4e10f5f4f00271feccbbae
Mohannad <moathaza@gmail.com>
2025-02-21 05:39:49 -0800

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOMWwG8WZri2ii4pDSvTdapkbnBJMfA-4",
-  authDomain: "poopopu-2a65b.firebaseapp.com",
-  projectId: "poopopu-2a65b",
-  storageBucket: "poopopu-2a65b.firebasestorage.app",
-  messagingSenderId: "326639379667",
-  appId: "1:326639379667:web:b8ea84c7feefd6264aa026",
-  measurementId: "G-3FYJBGX7D5"
+   apiKey: "AIzaSyD9M4S1blhwbJQ5FUXBkqzv5lsE_3QR4mQ",
+  authDomain: "amer-98afb.firebaseapp.com",
+  projectId: "amer-98afb",
+  storageBucket: "amer-98afb.firebasestorage.app",
+  messagingSenderId: "492435134855",
+  appId: "1:492435134855:web:86d3abd762bdc1856e5b22",
+  measurementId: "G-1YBB0DGJ9L"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
